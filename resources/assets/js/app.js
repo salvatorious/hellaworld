c27f9ddc203176feb323e64803ccf13b5fa30186
salvatorious <salvatore.the.candela@gmail.com>
2017-09-13 20:44:58 -0700

    Vue phone number input and login button toggle

---
+++ resources/assets/js/app.js
@@ -0,0 +1,34 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+import Cleave from 'vue-cleave';
+require('cleave.js/dist/addons/cleave-phone.us');
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+
+Vue.component('example', require('./components/Example.vue'));
+Vue.component('login-box', require('./components/LoginBox.vue'));
+Vue.component('cleave', Cleave);
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hella world, yo.'
+    },
+    computed: {
+        reversedMessage() {
+            return this.message.split('').reverse();
+        }
+    }
+});
---
+++ resources/assets/js/components/LoginBox.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+            <div class="panel-heading">Enter a phone number to log in</div>
+            <div class="panel-body">
+                <cleave placeholder="Enter your phone number" v-model="phone" :options="{ phone: true, phoneRegionCode: 'US' }" class="form-control"></cleave>
+            </div>
+            <div class="panel-body">
+                <div v-if="loggedIn">
+                    <h1>Yay! You're logged in!</h1>
+                    <button v-on:click="logoutClick" type="submit" class="btn btn-primary">
+                        Logout
+                    </button>
+                </div>
+                <div v-else>
+                    <button v-on:click="loginClick" type="submit" class="btn btn-primary">
+                        Login
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+                phone: '',
+                loggedIn: false
+            }
+        },
+        mounted() {
+            console.log('Login button mounted.');
+        },
+        methods: {
+            loginClick() {
+                this.loggedIn = true;
+                // this.$emit(loginClicked);
+            },
+            logoutClick() {
+                this.loggedIn = false;
+                // this.$emit(logoutClicked);
+            }
+        }
+    }
+</script>
