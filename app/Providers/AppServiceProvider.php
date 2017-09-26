c88e8d80a6f0a5459c29ef3a4ab9a3a884c8a2f5
Rand <rand@dottrombone.com>
2017-09-26 14:33:29 -0700

    Changed app/Providers/AppServiceProvider.php.  Cashier needs this to run its migrations.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Rand, cashier needs this to run its migration
+		Schema::defaultStringLength(191);
+		
     }
 
     /**
